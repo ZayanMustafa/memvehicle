5b4ad400d94e243fefad7ccf9fcba4137a69d7f5
Zayyan Mustafa <mustafazayyan13@gmail.com>
2025-04-21 02:38:20 +0500

    Work

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import CTASection from '@/component/CTASection'
 import Footer from '@/component/Footer'
 import HowItWorks from '@/component/HowItWork'
 import InspectionReports from '@/component/Inspectionreports'
+import Navbar from '@/component/Navbar'
 import Testimonials from '@/component/Testimonials'
 import HeroSection from '@/section/hero'
 import ServicesSection from '@/section/service'
@@ -11,6 +12,7 @@ import StatsSection from '@/section/states'
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <HeroSection/>
       <ServicesSection/>
       <InspectionReports/>
